590e10d14822990ada3f869f4be28046039e4519
tony.tosi <tony.tosi@pima.gov>
2023-08-01 15:24:06 -0700

    add todo reqs diagram

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -15,8 +15,8 @@
         <el-button class="" type="primary" @click="add">ADD</el-button>
       </div>
       <div class="flex-col border rounded-md overflow-auto h-[400px]">
-        <!-- if there are 1 or more todos, list them out using v-for using the todo-item component -->
-        <!-- :todo="todo" is a prop which is sent to the TodoItem component -->
+        <!-- if there are 1 or more todos, list them out using v-for using todo-item component -->
+        <!-- :todo="todo" is a prop which is sent to TodoItem component -->
         <!-- @complete and @remove the methods that're called when the event is emitted from the child -->
         <div v-if="todos.length > 0">
           <todo-item
@@ -42,31 +42,32 @@ import TodoItem from "@/components/TodoItem.vue";
 import { useNotification } from "@/composables/useNotification";
 import { useShowConfirmDialog, confirmTitle, confirmMessage } from "@/composables/useConfirmDialog";
 
-const todoStore = useTodoStore(); // this holds the actions in the store
-const { todo, todos } = storeToRefs(todoStore); // destructor for the props in the store
+const todoStore = useTodoStore(); // this holds the actions in the todoStore
+const { todo, todos } = storeToRefs(todoStore); // destructor for the props in todoStore
 
 // creates a reactive reference for the v-model in the input
 const title = ref<string>("");
 
-// call the todoStore to get the list of todos from the api
+// call todoStore to get list of todos from the api
+// load is just a wrapper to avoid using <Suspense></Suspense>
 const load = async () => {
   await todoStore.list();
 };
 load();
 
 const add = () => {
-  if (title.value.trim().length === 0) return; //don't allow adding empty todo
+  if (title.value.trim().length === 0) return; //don't allow adding an empty todo
 
   todo.value = new Todo(); // create a new todo in the store
   todo.value.title = title.value; // use .value on refs
-  const id: any = todoStore.create(todo.value); // for new todos, the id is returned from the api
+  const id: any = todoStore.create(todo.value); // for new todos, id is returned from the api
   todo.value.id = id;
-  todos.value.unshift(todo.value); // add new todo to top of the list in the ui
+  todos.value.unshift(todo.value); // add new todo to top of list in the ui
 
-  title.value = "";
+  title.value = ""; // reset textbox
 };
 
-// update todo and call update from the todo store
+// update todo and call update from todo store
 const complete = (todo: Todo) => {
   todo.completed = true;
   todo.completedDate = new Date();
@@ -76,11 +77,11 @@ const complete = (todo: Todo) => {
 const remove = async (todo: Todo) => {
   confirmTitle.value = "Delete";
   confirmMessage.value = "Are you sure you want to delete this todo?";
-  const response = await useShowConfirmDialog(); // await for user input from the confirmation dialog
+  const response = await useShowConfirmDialog(); // await for user input from confirmation dialog
 
   if (response) {
     await todoStore.delete(todo.id);
-    todos.value.splice(todos.value.indexOf(todo), 1); // remove the todo from the list in the ui
+    todos.value.splice(todos.value.indexOf(todo), 1); // remove todo from list in ui
 
     // show success notification
     useNotification.type = "success";
@@ -88,7 +89,7 @@ const remove = async (todo: Todo) => {
   }
 };
 
-// returns a sorted list that shows the completed todos at the bottom
+// returns a sorted list that shows completed todos at the bottom
 const sortedTodos = computed(() => {
   return todos.value.sort((a: any, b: any) => a.completed - b.completed);
 });
